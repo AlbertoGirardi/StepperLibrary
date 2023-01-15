b5311c49b50d7fbccd7220c836a2182d4fb3c41a
Alberto Girardi <alberto.gir747@gmail.com>
2023-01-15 16:45:52 +0100

    batch installazion

---
+++ src/install_library.bat
@@ -0,0 +1,6 @@
+echo "installazione libreria, USARE SOLO IN SVILUPPO"
+
+del C:\Users\alber\OneDrive\Documenti\Arduino\libraries\StepperF\StepperF.cpp
+del C:\Users\alber\OneDrive\Documenti\Arduino\libraries\StepperF\StepperF.cpp
+copy StepperF.cpp C:\Users\alber\OneDrive\Documenti\Arduino\libraries\StepperF
+copy StepperF.h C:\Users\alber\OneDrive\Documenti\Arduino\libraries\StepperF
